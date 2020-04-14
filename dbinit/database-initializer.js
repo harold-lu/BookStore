c77317b2e4848ba083ed1eb37bff702b654a5d3e
harold lo <harold.lu123@gmail.com>
2020-04-14 16:44:13 -0400

    Project update

--- dbinit/database-initializer.js
+++ dbinit/database-initializer.js
@@ -30,90 +30,89 @@ initOptions = {
   }
 };
 pgp = require('pg-promise')(initOptions);
-db = pgp('postgres://postgres:Chinago88@localhost:5432/project');
+db = pgp('postgres://<db_username>:<db_password>@localhost:5432/<db_name>');
 
 
-//
-// for(let i = 0; i < book.length; i++){
-//   db.one({
-//     text: book[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//         console.log('ERROR: ' + error);
-//     });
-// }
-//
-// for(let i = 0; i < publisher.length; i++){
-//   db.one({
-//     text: publisher[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//
-//     });
-// }
-//
-// for(let i = 0; i < author.length; i++){
-//   db.one({
-//     text: author[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//
-//     });
-// }
-// //
-// for(let i = 0; i < published_by.length; i++){
-//   db.one({
-//     text: published_by[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//
-//     });
-// }
-//
-// for(let i = 0; i < written_by.length; i++){
-//   db.one({
-//     text: written_by[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//
-//     });
-// }
 
-// for(let i = 0; i < bank.length; i++){
-//   db.one({
-//     text: bank[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
-//
-//     });
-// }
+for(let i = 0; i < book.length; i++){
+  db.one({
+    text: book[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+    });
+}
 
-// for(let i = 0; i < publisher_bank.length; i++){
-//   db.one({
-//     text: publisher_bank[i] // can also be a QueryFile object
-// })
-//     .then(user => {
-//         // user found;
-//     })
-//     .catch(error => {
+for(let i = 0; i < publisher.length; i++){
+  db.one({
+    text: publisher[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
+
+for(let i = 0; i < author.length; i++){
+  db.one({
+    text: author[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
 //
-//     });
-// }
+for(let i = 0; i < published_by.length; i++){
+  db.one({
+    text: published_by[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
+
+for(let i = 0; i < written_by.length; i++){
+  db.one({
+    text: written_by[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
+
+for(let i = 0; i < bank.length; i++){
+  db.one({
+    text: bank[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
+
+for(let i = 0; i < publisher_bank.length; i++){
+  db.one({
+    text: publisher_bank[i] // can also be a QueryFile object
+})
+    .then(user => {
+        // user found;
+    })
+    .catch(error => {
+
+    });
+}
